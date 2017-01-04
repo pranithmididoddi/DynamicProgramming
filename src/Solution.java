1a8b1b1969b1167ddb60ea28a79b4f9222a709bf
pranithmididoddi <pranith13@gmail.com>
2017-01-03 20:36:27 -0600

    Dynamic Programming Most popular-->Longest Increasing Subsequence

--- src/Solution.java
+++ src/Solution.java
@@ -2,4 +2,56 @@
  * Created by Pranith on 1/3/17.
  */
 public class Solution {
+
+    public static int LIS(int[] nums) {
+        int result = 0;
+        int[] res = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    res[i] = Math.max(res[i], res[j] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < res.length; i++) {
+            if (res[i] > result) {
+                result = res[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] myarray = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(LIS(myarray));
+    }
 }
+
+/** for interviewBit   int result=0;
+    int[] res=new int[nums.size()];
+
+        for(int i=0;i<nums.size();i++){
+        res[i]=1;
+        for(int j=0;j<i;j++){
+            if(nums.get(i)>nums.get(j)){
+                res[i]=Math.max(res[i],res[j]+1);
+            }
+        }
+    }
+
+        for(int i=0;i<res.length;i++){
+        if(res[i]>result){
+            result=res[i];
+        }
+    }
+        return result;
+
+}
+
+    }
+
+    **/
+
